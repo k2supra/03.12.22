029c6126acad57f1d953d50270c48b9d2fafdb43
k2supra <andrijkorolevic@gmail.com>
2022-12-03 09:06:32 +0200

    Created new feature/hw_ex_1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello (=||=)")
+try:
+    with open("task.txt", "w") as file:
+        file.write("hellomy- world for everyyday on the beach and Mercury`s space potato")
+
+    with open("task.txt", "r") as file:
+        for line in file:
+            print(line, end="")
+except Exception as ex:
+    print(f"Error: {ex}")
