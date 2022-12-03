335c2f9553cbff6597545348ef5570d995e27dbc
k2supra <andrijkorolevic@gmail.com>
2022-12-03 09:46:35 +0200

    Created new feature/hw_ex_1

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ try:
 
     with open("task.txt", "r") as file:
         for line in file:
-            print(line, end="")
+            print("Full text: ", line, end=" ")
+    print()
+
+    k = line.split(' ')
+    for item in k:
+        l = len(item)
+        #print(l, end=" ")
+        if l > 7:
+            print("\033[1;32;40m Words which have 7+ letters:\033[0m ", item)
 except Exception as ex:
     print(f"Error: {ex}")
